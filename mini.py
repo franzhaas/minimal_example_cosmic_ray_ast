6cb11424fd92403f9ee9a70deb4056eed692b302
Franz Haas <franz.haas@ams.com>
2018-03-05 21:57:12 +0100

    - minimized further

--- mini.py
+++ mini.py
@@ -1,17 +1,4 @@
-from itertools import repeat, chain
-from typing import Sequence, Iterable, List, Callable, Any, Type
-from functools import wraps
+import typing
 
-
-
-def _check_value(item: int) -> int:
-    if item & 0xFF != item:
-        raise ValueError('Trying to mem dump non byte data (0x00 - 0xFF)')
-    return item
-
-def transform_sequence_of_bytes_to_string_represented_mem(data: Iterable[int]) -> str:
-    return ' '.join('{:02X}'.format(_check_value(item)) for item in data)
-
-
-def transform_string_represented_mem_to_int_sequence(string_rep: str) -> List[int]:
-    return [int(item, 16) for item in string_rep.split()]
+def adding(a, b):
+    return a + b
--- test_mini.py
+++ test_mini.py
@@ -2,9 +2,10 @@ from hypothesis import given, example
 from hypothesis.strategies import integers, lists
 
 import mini
+import sys
 
+sys.setrecursionlimit(3000)
 
-@given(lists(integers(min_value=0x00, max_value=0xFF)))
-def test_reversible_sequence_conv(data):
-    string_rep = mini.transform_sequence_of_bytes_to_string_represented_mem(data)
-    assert data == mini.transform_string_represented_mem_to_int_sequence(string_rep)
+@given(integers(min_value=0x00, max_value=0xFF),integers(min_value=0x00, max_value=0xFF))
+def test_reversible_sequence_conv(a, b):
+    assert a + b == mini.adding(a, b)
